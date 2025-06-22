4ee1a6da70f6a2f3bc55d2338bfb22a3e4ebe79c
Zackary(Redmi) <3173791351@qq.com>
2025-06-22 20:22:50 +0800

    three color triangle

--- src/shaders/shader.h
+++ src/shaders/shader.h
@@ -4,21 +4,21 @@
 inline auto vertexShaderSource =
         "#version 330 core\n"
         "layout (location = 0) in vec3 aPos;\n"
-        "out vec4 vertexColor;\n"
+        "layout (location = 1) in vec3 aColor;\n"
+        "out vec3 ourColor;\n"
         "void main()\n"
         "{\n"
         "   gl_Position = vec4(aPos,1.0);\n"
-        "   vertexColor = vec4(0.5,0.0,0.0,1.0);\n"
+        "   ourColor = aColor;\n"
         "}\0";
 
 inline auto fragmentShaderSource =
         "#version 330 core\n"
         "out vec4 FragColor;\n"
-        "in vec4 vertexColor;\n"
-        "uniform vec4 ourColor;\n"
+        "in vec3 ourColor;\n"
         "void main()\n"
         "{\n"
-        "   FragColor = ourColor;\n"
+        "   FragColor = vec4(ourColor,1.0);\n"
         "}\n";
 
 inline unsigned int genVertexShader() {
