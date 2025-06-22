9c1b6b57a9b511b86d5a3c44646685c79206acdc
Zackary(Redmi) <3173791351@qq.com>
2025-06-22 16:57:23 +0800

    3d life game first commit(first triangle)

---
+++ src/shaders/shader.h
@@ -0,0 +1,62 @@
+#pragma once
+#include <iostream>
+
+inline auto vertexShaderSource =
+        "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x,aPos.y,aPos.z,1.0);\n"
+        "}\0";
+
+inline auto fragmentShaderSource =
+        "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(1.0f,0.5f,0.2f,1.0f);\n"
+        "}\n";
+
+inline unsigned int genVertexShader() {
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+    int vertexCompileSuccess;
+    char infolog[512];
+    glGetShaderiv(vertexShader,GL_COMPILE_STATUS, &vertexCompileSuccess);
+    if (not vertexCompileSuccess) {
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infolog);
+        std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infolog << std::endl;
+    } else {
+        std::cout << "INFO::SHADER::VERTEX::COMPILATION_SUCCESS\n" << std::endl;
+    }
+    return vertexShader;
+}
+
+inline unsigned int genFragmentShader() {
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+    int fragmentCompileSuccess;
+    char infolog[512];
+    glGetShaderiv(fragmentShader,GL_COMPILE_STATUS, &fragmentCompileSuccess);
+    if (not fragmentCompileSuccess) {
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infolog);
+        std::cerr << "ERROR::SHADER::fragment::COMPILATION_FAILED\n" << infolog << std::endl;
+    } else {
+        std::cout << "INFO::SHADER::fragment::COMPILATION_SUCCESS\n" << std::endl;
+    }
+    return fragmentShader;
+}
+
+inline unsigned int genShaderProgram() {
+    const unsigned int vertexShader = genVertexShader();
+    const unsigned int fragmentShader = genFragmentShader();
+    const unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    return shaderProgram;
+}
