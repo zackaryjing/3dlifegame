415eaf9508028f11381403285a9560ebf921d372
Jiang <108873506+zhiyangjing@users.noreply.github.com>
2025-08-06 23:59:00 +0800

    wip: render first cube

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <unistd.h>
 #include "init.h"
+#include "model/shapes.h"
 #include "shaders/shader.h"
 #include "texture/load_texture.h"
 #ifndef GLSL_DIR
@@ -34,13 +35,6 @@ int main() {
     }
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    float vertices[] = {
-            0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0, 1.0f, //
-            0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0, 0.0f, //
-            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0, 0.0f, //
-            -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0, 1.0f,
-    };
-
     int nrAttributes;
     glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
     cout << "Maximum nr of vertex attributes supported: " << nrAttributes
@@ -60,35 +54,40 @@ int main() {
     unsigned int VBO;
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Cube::vertices), Cube::vertices,
+                 GL_STATIC_DRAW);
 
     unsigned int EBO;
     glGenBuffers(1, &EBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Cube::indices), Cube::indices,
                  GL_STATIC_DRAW);
 
 
     // vertex attributes pointers
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
-                          reinterpret_cast<void *>(0 * sizeof(float)));
+    glVertexAttribPointer(Cube::vertices_index, Cube::vertices_size, GL_FLOAT,
+                          GL_FALSE, Cube::vertices_stride,
+                          Cube::vertices_pointer);
     // vertex attribute location
     glEnableVertexAttribArray(0);
 
     // vertex attributes pointers
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
-                          reinterpret_cast<void *>(3 * sizeof(float)));
+    glVertexAttribPointer(Cube::coord_index, Cube::coord_size, GL_FLOAT,
+                          GL_FALSE, Cube::coord_stride,
+                          Cube::coord_pointer);
     // vertex attribute location
     glEnableVertexAttribArray(1);
 
-    // vertex attributes pointers
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
-                          reinterpret_cast<void *>(6 * sizeof(float)));
-    // vertex attribute location
-    glEnableVertexAttribArray(2);
+    // // vertex attributes pointers
+    // glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+    //                       reinterpret_cast<void *>(6 * sizeof(float)));
+    // // vertex attribute location
+    // glEnableVertexAttribArray(2);
+    glEnable(GL_DEPTH_TEST);
 
 
-    // const Shader ourShader("./shaders/shader.fs", "./shaders/shader.vs");
+    // const Shader ourShader("./shaders/shader.fs",
+    // "./shaders/shader.vs");
     const Shader ourShader(GLSL_DIR "shader.vs", GLSL_DIR "shader.fs");
 
     ourShader.use();
@@ -100,9 +99,6 @@ int main() {
     glBindVertexArray(VAO);
     ourShader.setInt("ourTexture", 0);
 
-    glm::mat4 model = glm::mat4(1.0f);
-    model = glm::rotate(model, glm::radians(-55.0f),
-                        glm::vec3(1.0f, 0.0f, 0.0f));
 
     glm::mat4 view = glm::mat4(1.0f);
     view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
@@ -114,8 +110,12 @@ int main() {
     // glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
     // glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
     while (not glfwWindowShouldClose(window)) {
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::rotate(model, (float) glfwGetTime() * glm::radians(50.0f),
+                            glm::vec3(0.5f, 1.0f, 0.0f));
+
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         processInput(window);
 
         const auto timeValue = static_cast<float>(glfwGetTime());
@@ -128,7 +128,7 @@ int main() {
         // draw triangles
         // glDrawArrays(GL_TRIANGLES, 0, 6);
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, Cube::indices_cnt, GL_UNSIGNED_INT, 0);
 
 
         glfwSwapBuffers(window);
--- src/model/shapes.h
+++ src/model/shapes.h
@@ -1,6 +1,6 @@
 
 struct Cube {
-    float vertex[] = {
+    static inline float vertices[] = {
             -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, //
             0.5f,  -0.5f, 0.5f,  1.0f, 0.0f, //
             0.5f,  0.5f,  0.5f,  1.0f, 1.0f, //
@@ -10,7 +10,7 @@ struct Cube {
             0.5f,  0.5f,  -0.5f, 1.0f, 1.0f, //
             -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f, //
     };
-    float indices[] = {
+    static inline int indices[] = {
             0, 1, 2, //
             2, 3, 0, //
             6, 5, 4, //
@@ -23,7 +23,33 @@ struct Cube {
 
             4, 5, 1, //
             1, 0, 4, //
-            7, 4, 2, //
+            7, 3, 2, //
             2, 6, 7, //
     };
+
+    // typedef void (APIENTRYP PFNGLVERTEXATTRIBPOINTERPROC)(GLuint index, GLint
+    // size, GLenum type, GLboolean normalized, GLsizei stride, const void
+    // *pointer);
+    static inline GLuint vertices_index = 0;
+    static inline GLint vertices_size = 3;
+    static inline GLsizei vertices_stride = 5 * sizeof(float);
+    static inline auto vertices_pointer =
+            reinterpret_cast<void *>(0 * sizeof(float));
+    // texture coordinate
+    static inline GLuint coord_index = 1;
+    static inline GLint coord_size = 2;
+    static inline GLsizei coord_stride = 5 * sizeof(float);
+    static inline auto coord_pointer =
+            reinterpret_cast<void *>(3 * sizeof(float));
+    static inline GLsizei vertices_cnt = 8;
+    static inline GLsizei indices_cnt = 36;
 };
+
+// -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, //
+// 0.5f,  -0.5f, 0.5f,  1.0f, 0.0f, //
+// 0.5f,  0.5f,  0.5f,  1.0f, 1.0f, //
+// -0.5f, 0.5f,  0.5f,  0.0f, 1.0f, //
+// -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, //
+// 0.5f,  -0.5f, -0.5f, 1.0f, 0.0f, //
+// 0.5f,  0.5f,  -0.5f, 1.0f, 1.0f, //
+// -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f, //
